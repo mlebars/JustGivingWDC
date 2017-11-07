1998ff77af6b4ce56b8d5c3b15ba7b7a88e7ba5c
Marie <mlebars@users.noreply.github.com>
2017-11-07 20:40:27 +0000

    Update justgivingdonations.js

--- justgivingdonations.js
+++ justgivingdonations.js
@@ -6,7 +6,7 @@
 	        id: "id",
 	        dataType: tableau.dataTypeEnum.int
 	    }, {
-	        id: "charityid",
+	        id: "charityId",
 	        dataType: tableau.dataTypeEnum.int
 	    }, {
 	        id: "donationDate",
@@ -60,48 +60,44 @@
 	};
 
 	myConnector.getData = function(table, doneCallback) {
-	    $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
-	        var feat = resp.features,
-	            tableData = [];
 
-	        // Iterate over the JSON object
-	        for (var i = 0, len = feat.length; i < len; i++) {
-	            tableData.push({
-	                "id": feat[i].id,
-	                "mag": feat[i].properties.mag,
-	                "title": feat[i].properties.title,
-	                "location": feat[i].geometry
-	            });
-	        }
-
-	        table.appendRows(tableData);
-	        doneCallback();
-	    });
-	};
+	    var donations = {};
+	    var tableData = [];
+	    $.ajax({
+    		type: "GET",
+   		url: "https://api.justgiving.com/7e017f27/v1/fundraising/pages/jenny-houghton1/donations",
+    		dataType : 'json'
+	    }).done(function (obj) {
+  		donations = obj;
+		var x = donations.donations;
+  		for (var i = 0, len = x.length; i < len; i++) {
+		    tableData.push({
+			  "id": x[i].id,
+			  "charityId": x[i].charityId,
+			  "donationDate": x[i].donationDate,
+			  "donorDisplayName": x[i].donorDisplayName,
+			  "currencyCode": x[i].currencyCode,
+			  "amount": x[i].amount,
+			  "donorLocalCurrencyCode": x[i].donorLocalCurrencyCode,
+			  "donorLocalAmount": x[i].donorLocalAmount,
+			  "donationRef": x[i].donationRef,
+			  "thirdPartyReference": x[i].thirdPartyReference,
+			  "image": x[i].image,
+			  "message": x[i].message
+	             });
+		}
+	table.appendRows(tableData);
+	doneCallback();
+	console.log(tableData);
+	});
+    };
 
     tableau.registerConnector(myConnector);
 
 	$(document).ready(function () {
     	$("#submitButton").click(function () {
-        	tableau.connectionName = "USGS Earthquake Feed";
+        	tableau.connectionName = "Just Giving - Fundraiser Donations";
         	tableau.submit();
     	});
 });
 })();
-
-
-
-/* 
-
-var donations = {};
-
-$.ajax({
-    type: "GET",
-    url: "https://api.justgiving.com/7e017f27/v1/fundraising/pages/jenny-houghton1/donations",
-    dataType : 'json'
-}).done(function (obj) {
-  donations = obj;
-  console.log(donations.donations["0"].amount);
-});
-
-*/
